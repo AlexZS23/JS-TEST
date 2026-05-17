5cd2d7c894c3bbb4a2ae2f954bb88dbc47a9199c
Oleksii Zatoliuk <oleksii.z@smart-gravity.com>
2026-05-17 20:51:37 +0300

    Lesson3-HW - JS Colors

--- nodeBasics/index.js
+++ nodeBasics/index.js
@@ -3,4 +3,5 @@ import chalk from "chalk";
 let name = 'Alex'
 let birthDate = 'too many years ago'
 
-console.log(chalk.blue('Who was born ' + birthDate + '? ') + chalk.yellow(name))
+console.log(chalk.blue('Who was born ' + birthDate + '? ')) 
+console.log(chalk.yellow(name));
